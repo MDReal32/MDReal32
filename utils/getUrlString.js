913135829c99478efbb4121d33976bd242cb8272
MDReal <veysaliyev00@gmail.com>
2021-08-12 20:54:09 +0400

    Fix #v2

--- utils/getUrlString.js
+++ utils/getUrlString.js
@@ -5,7 +5,7 @@ module.exports.getUrlString = name => {
     return `<img src="${externalUrls[name]}" style="display: inline" alt="${name}" width="25" />`;
 
   if (["Jenkins", "Postman"].includes(name))
-    return `<img src="https://raw.githubusercontent.com/MDReal32/MDReal32/master/assets/${name.toLowerCase()}.svg" alt="${name}" width="25" />`;
+    return `<img src="https://raw.githubusercontent.com/MDReal32/MDReal32/master/assets/${name.toLowerCase()}.svg" style="display: inline" alt="${name}" width="25" />`;
 
   return `![${name}](https://icongr.am/devicon/${name.toLowerCase()}-original.svg?size=25&color=aabbcc)`;
 };
