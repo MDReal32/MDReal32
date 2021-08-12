e4beaebe5bc40e713aa3d34e02555c8d3002d83d
MDReal <veysaliyev00@gmail.com>
2021-08-12 20:52:01 +0400

    View Problem Fixed

--- utils/getUrlString.js
+++ utils/getUrlString.js
@@ -2,7 +2,7 @@ const { externalUrls } = require("./const");
 
 module.exports.getUrlString = name => {
   if (Object.keys(externalUrls).includes(name))
-    return `<img src="${externalUrls[name]}" alt="${name}" width="25" />`;
+    return `<img src="${externalUrls[name]}" style="display: inline" alt="${name}" width="25" />`;
 
   if (["Jenkins", "Postman"].includes(name))
     return `<img src="https://raw.githubusercontent.com/MDReal32/MDReal32/master/assets/${name.toLowerCase()}.svg" alt="${name}" width="25" />`;
