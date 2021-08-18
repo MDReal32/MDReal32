fad37653b38f5347e3b24dbf7675003328fd12aa
MDReal <veysaliyev00@gmail.com>
2021-08-19 02:28:37 +0400

    IDEA added to apps list

--- utils/const.js
+++ utils/const.js
@@ -29,6 +29,7 @@ module.exports = {
   os: [
     "Webstorm",
     "Pycharm",
+    "IDEA",
     "VSCode",
     "Linux",
     "Windows",
@@ -76,6 +77,7 @@ module.exports = {
     Kubernetes: `https://upload.wikimedia.org/wikipedia/commons/3/39/Kubernetes_logo_without_workmark.svg`,
     VSCode: `https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg`,
     Webstorm: `https://upload.wikimedia.org/wikipedia/commons/c/c0/WebStorm_Icon.svg`,
+    IDEA: `https://upload.wikimedia.org/wikipedia/commons/9/9c/IntelliJ_IDEA_Icon.svg`,
     Windows: `https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Windows_logo_-_2012_%28dark_blue%29.svg/1024px-Windows_logo_-_2012_%28dark_blue%29.svg.png`,
     Pycharm: `https://upload.wikimedia.org/wikipedia/commons/1/1d/PyCharm_Icon.svg`,
     Linux: `https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/374px-Tux.svg.png`,
