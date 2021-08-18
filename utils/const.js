7dd68ddffb0fa2caf0f6ea534350ab661e21a640
MDReal <veysaliyev00@gmail.com>
2021-08-19 01:14:10 +0400

    Modified knows

--- utils/const.js
+++ utils/const.js
@@ -1,9 +1,9 @@
 module.exports = {
   programmingLanguages: ["JavaScript", "TypeScript", "__", "Python", "Java"],
   frontFrameworks: ["HTML5", "CSS3", "SASS", "__", "Webpack", "ViteJS", "__", "React", "VueJS"],
-  backFrameworks: ["NodeJS", "Express", "__", "Sequelize", "Django"],
-  os: ["Webstorm", "Pycharm", "VSCode", "__", "Linux", "Windows", "__", "Postman"],
-  devOps: ["Git", "Jenkins", "Docker", "Kubernetes"],
+  backFrameworks: ["NodeJS", "Express", "Sequelize", "__", "Django"],
+  os: ["Webstorm", "Pycharm", "VSCode", "Linux", "Windows", "__", "Postman", "JIRA", "Slack"],
+  devOps: ["Git", "GitHub", "GitLab", "BitBucket", "__", "Docker", "Kubernetes", "__", "Jenkins"],
   socialNetworks: {
     LinkedIn: `https://www.linkedin.com/in/mdreal32`,
     Instagram: `https://instagram.com/mdreal32`,
@@ -16,6 +16,11 @@ module.exports = {
     Webstorm: `https://upload.wikimedia.org/wikipedia/commons/c/c0/WebStorm_Icon.svg`,
     Windows: `https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Windows_logo_-_2012_%28dark_blue%29.svg/1024px-Windows_logo_-_2012_%28dark_blue%29.svg.png`,
     Pycharm: `https://upload.wikimedia.org/wikipedia/commons/1/1d/PyCharm_Icon.svg`,
-    Linux: `https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/374px-Tux.svg.png`
+    Linux: `https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/374px-Tux.svg.png`,
+    JIRA: `https://upload.wikimedia.org/wikipedia/commons/4/4a/Jira_Software%402x-blue.png`,
+    Slack: ``
+  },
+  widths: {
+    JIRA: 175
   }
 };
--- utils/getUrlString.js
+++ utils/getUrlString.js
@@ -1,13 +1,14 @@
-const { externalUrls } = require("./const");
+const { externalUrls, widths } = require("./const");
 
 module.exports.getUrlString = (name, isHTML = false) => {
   if (name === "__") return isHTML ? "<br />" : `\n`;
 
+  const width = widths[name] || 25;
   if (Object.keys(externalUrls).includes(name))
-    return `<img src="${externalUrls[name]}" style="display: inline-block" alt="${name}" width="25" />`;
+    return `<img src="${externalUrls[name]}" style="display: inline-block" alt="${name}" width="${width}" title="${name}" />`;
 
-  if (["Jenkins", "Postman"].includes(name))
-    return `<img src="https://raw.githubusercontent.com/MDReal32/MDReal32/master/assets/${name.toLowerCase()}.svg" style="display: inline-block" alt="${name}" width="25" />`;
+  if (["Jenkins", "Postman", "Slack"].includes(name))
+    return `<img src="https://raw.githubusercontent.com/MDReal32/MDReal32/master/assets/${name.toLowerCase()}.svg" style="display: inline-block" alt="${name}" width="${width}" title="${name}" />`;
 
-  return `![${name}](https://icongr.am/devicon/${name.toLowerCase()}-original.svg?size=25&color=aabbcc)`;
+  return `![${name}](https://icongr.am/devicon/${name.toLowerCase()}-original.svg?size=25&color=aabbcc "${name}")`;
 };
