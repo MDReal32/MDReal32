5049693dea95ecbbae05968fabeef51a4f953828
MDReal <veysaliyev00@gmail.com>
2021-08-19 01:16:01 +0400

    Slack Url Added

--- utils/const.js
+++ utils/const.js
@@ -17,10 +17,10 @@ module.exports = {
     Windows: `https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Windows_logo_-_2012_%28dark_blue%29.svg/1024px-Windows_logo_-_2012_%28dark_blue%29.svg.png`,
     Pycharm: `https://upload.wikimedia.org/wikipedia/commons/1/1d/PyCharm_Icon.svg`,
     Linux: `https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/374px-Tux.svg.png`,
-    JIRA: `https://upload.wikimedia.org/wikipedia/commons/4/4a/Jira_Software%402x-blue.png`,
-    Slack: ``
+    JIRA: `https://upload.wikimedia.org/wikipedia/commons/4/4a/Jira_Software%402x-blue.png`
   },
   widths: {
-    JIRA: 175
+    JIRA: 175,
+    Slack: 80
   }
 };
